54c34e573af13971ec47e10cc149315d03935652
UserGitHubBeta <itslxc.agst@proton.me>
2023-02-10 13:53:12 +0100

    Update Project

---
+++ PHP/exo-clients/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8" />
+	<link rel="stylesheet" href="style.css" />
+	<title>HTML 5 et CSS 3: Exercice 6</title>
+</head>
+
+<body>
+	<header>
+		<nav>
+			<ul>
+				<li><a href="index.html">Accueil</a></li>
+				<li><a href="flexbox_images.html">Flexbox et images</a></li>
+				<li><a href="formulaire.html">Formulaire</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section>
+		<article>
+			<h1>World Wide Web</h1>
+			<p>Le <span class="mise_en_evidence">World Wide Web (WWW)</span>, littéralement la « toile (d’araignée) mondiale », communément appelé le web, et parfois la toile, est un système hypertexte public fonctionnant sur internet qui permet de consulter, avec un navigateur, des pages accessibles sur des sites. L’image de la toile d'araignée vient des hyperliens qui lient les pages web entre elles.</p>
+			<p>Le Web n’est qu’une des applications d’Internet. D’autres applications sont le courrier électronique, la messagerie instantanée et Usenet. Le Web a été inventé plusieurs années après Internet, mais c’est lui qui a rendu les médias grand public attentifs à Internet. Depuis, le Web est fréquemment confondu avec Internet ; en particulier, le mot Toile est souvent utilisé dans les textes non techniques sans qu’il soit clairement exprimé s’il désigne le Web ou Internet.</p>
+
+			<!-- Partie n°1: à afficher que si l'utilisateur est authentifié -->
+			<h2>Historique</h2>
+			<p><strong>Tim Berners-Lee</strong> travaille comme informaticien dans le bâtiment 31 ( 46.2325, 6.0450 9) de l’Organisation européenne pour la recherche nucléaire (CERN), situé à la frontière franco-suisse, lorsqu’il propose, en 1989, de créer un système hypertexte distribué sur le réseau informatique pour que les collaborateurs puissent partager les informations au sein du CERN. Cette même année, les responsables du réseau du CERN décident d’utiliser le protocole de communication TCP/IP et le CERN ouvre sa première connexion extérieure avec Internet.</p>
+			<p>L’année suivante, l’ingénieur système <strong>Robert Cailliau</strong> se joint au projet d’hypertexte au CERN, immédiatement convaincu de son intérêt, et se consacre énergiquement à sa promotion. <strong>Tim Berners-Lee et Robert Cailliau sont reconnus comme les deux personnes à l’origine du World Wide Web</strong>.</p>
+			<p>Jusqu’en 1993, le web est essentiellement développé sous l’impulsion de Tim Berners-Lee et Robert Cailliau. Les choses changent avec l’apparition de NCSA Mosaic, un navigateur web développé par Eric Bina et Marc Andreessen au National Center for Supercomputing Applications (NCSA), dans l’Illinois. NCSA Mosaic jette les bases de l’interface graphique des navigateurs modernes et cause un accroissement exponentiel de la popularité du web. Le NCSA produit également le NCSA HTTPd, un serveur HTTP qui évoluera en Apache HTTP Server, le serveur HTTP le plus utilisé depuis 1996.</p>
+			<p>En 1994, Netscape Communications Corporation est fondée avec une bonne partie de l’équipe de développement de NCSA Mosaic. Sorti fin 1994, Netscape Navigator supplante NCSA Mosaic en quelques mois.</p>
+			<p>En 1995, Microsoft essaie de concurrencer Internet avec The Microsoft Network (MSN) et échoue. Fin 1995, après la sortie de Windows 95 sans le moindre navigateur web préinstallé, Microsoft lance avec Internet Explorer la guerre des navigateurs contre Netscape Navigator.</p>
+			<!-- FIN Partie n°1 -->
+
+			<h2>Évolution de l'usage</h2>
+			<p>En créant le logiciel WorldWideWeb, Tim Berners-Lee a créé à la fois le premier navigateur web et le premier éditeur web, car il voulait faire du web un média collaboratif, dans lequel tous les acteurs consultent et créent l'information. Cependant, le web s'est immédiatement orienté en un média de diffusion d'information global plutôt que de collaboration.</p>
+			<p>Dans la seconde moitié des années 1990, le web devient populaire, et toutes les grandes entreprises, organisations, écoles, administrations, ouvrent un site web. Les moteurs de recherche deviennent efficaces, notamment avec l'apparition d'Altavista en décembre 1995, et pour finir Google en 1998. Dans cette phase de développement du média, un flot d'information top-down prédomine : un site web est fait pour diffuser les informations de son propriétaire. Les interactions s'arrêtent souvent à la recherche et au commerce en ligne. Bien sûr, le courrier électronique, la messagerie instantanée, et les forums de discussion existaient depuis plus longtemps que le web, mais ils fonctionnaient principalement avec des protocoles et logiciels spécifiques.</p>
+
+			<!-- Partie n°2: à afficher que si l'utilisateur est authentifié -->
+			<h2>Web 2.0</h2>
+			<p>Avec les années 2000 les notions de blog, de wiki (Wikipedia anglophone en 2001) et de réseautage social (Myspace en 2003, Facebook en 2004) deviennent populaires. Le contenu généré par les utilisateurs se répand (Wikipédia, YouTube en 2005, Twitter en 2006). La technologie Ajax (1998, théorisée en 2005) commence à être largement utilisée pour créer des applications complètes qui tiennent dans une seule page web (Google Maps en 2004). L'expression Web 2.0, largement popularisée au milieu des années 2000, désigne cette transition dans le flux de l'information et la manière d'utiliser le web. Le succès de l'expression Web 2.0 a conduit de nombreuses personnes à appeler web 2.5, 3.0, 4.0, etc. leur vision du web de l'avenir.</p>
+			<p class="reference">Source : Wikipédia</p>
+			<!-- FIN Partie n°2 -->
+		</article>
+	</section>
+	<footer>
+		<p>(c) LAF, juillet 2014, dernière modification par CV le 21.09.2020</p>
+		<p>Classe passerelle</p>
+	</footer>
+</body>
+
+</html>
