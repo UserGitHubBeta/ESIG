adcf067387c3807508f6de5017e731c94206574e
UserGitHubBeta <itslxc.agst@proton.me>
2023-02-14 11:42:38 +0100

    Update Project Banking

--- JAVA/MyBank/src/Account.java
+++ JAVA/MyBank/src/Account.java
@@ -15,16 +15,63 @@ public class Account {
     public double getBalance() {
         return balance;
     }
+
+    public void credit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+        } else {
+            System.out.println("Error: cannot credit negative amount.");
+        }
+    }
+
+    public void debit(double amount) {
+        if (amount > 0 && balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Error: cannot debit negative amount or overdraft account.");
+        }
+    }
 }
 
 class AccountTest {
     public static void main(String[] args) {
         testerAffichage();
+        testerGetBalance();
+        testerCredit();
+        testerDebit();
+    }
+
+    public static void testerGetBalance() {
+        Account ClientA = new Account("007");
+        System.out.println("Après construction, le solde devriat être 0. Obtenu " + ClientA.getBalance());
+    }
+
+    public static void testerCredit() {
+        Account account1 = new Account("008");
+        account1.credit(50);
+        // "/t" permet d'aligner les éléments comme un tab
+        System.out.println("Après crédit de 50 le solde devrait être de 50. Obtenu: \t" + account1.getBalance());
+        account1.credit(20.5);
+        System.out.println("Après crédit de 20.5 le solde devrait être de 70.5 Obtenu: \t" + account1.getBalance());
+        System.out.println("\n");
+    }
+
+    public static void testerDebit() {
+        System.out.println("\n");
+        Account account1 = new Account("010");
+        System.out.println("Après débit de 50 le solde devrait être de -50. Obtenu: \t" + account1.getBalance());
+        account1.debit(50);
+        System.out.println("Après crédit de 50+20.5 le solde devrait être de -70.5 Obtenu: \t" + account1.getBalance());
+        account1.debit(20.5);
     }
 
     public static void testerAffichage() {
         Account account1 = new Account("A1");
         System.out.println("Voici mon compte: " + account1.toString());
-        System.out.println(getBalance());
+        System.out.println();
+    }
+
+    public static void testerCreditNegatif(double amount) {
+        System.out.println();
     }
 }
