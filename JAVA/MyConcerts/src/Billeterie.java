60902bc4a53297ec2428cfe9191ea436871b33d2
UserGitHubBeta <itslxc.agst@proton.me>
2023-02-27 22:39:11 +0100

    Update Billeterie.java

--- JAVA/MyConcerts/src/Billeterie.java
+++ JAVA/MyConcerts/src/Billeterie.java
@@ -1,5 +1,15 @@
 public class Billeterie {
+
+    public Billeterie() {
+        super();
+    }
+    
+    @Override
+    public String toString() {
+        return "Billeterie []";
+    }
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        System.out.println("VRRRROUMMM");
     }
 }
