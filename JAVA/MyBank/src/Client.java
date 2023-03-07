ed2bf85d15cb2184b880b7316db189d74f92790d
UserGitHubBeta <itslxc.agst@proton.me>
2023-03-07 13:26:22 +0100

    Update Client.java

--- JAVA/MyBank/src/Client.java
+++ JAVA/MyBank/src/Client.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class Client {
+    String accounts;
     String nomClient;
     String prenomClient;
     String identifiantClient;
@@ -24,6 +26,14 @@ public class Client {
 
         return nomClient + " " + prenomClient + " (" + identifiantClient + ")" + " " + dateMiseEnForme;
     }
+
+    public String getAccounts() {
+        return accounts;
+    }
+
+    public void addAccount(String accounts) {
+        System.out.println(accounts);
+    }
 }
 
 class ClientTest {
@@ -41,4 +51,29 @@ class ClientTest {
             System.out.println("Client " + i + " " + firstClient);
         }
     }
+
+    private static void testerCompteClient()
+    {
+        Client client = new Client(101,'Daniel','Smith',LocalDate.of(1980,6,5));
+        Account acc1 = new Account('A1');
+        client.addAccount(acc1);             
+        Account acc2 = new Account('A2');
+        client.addAccount(acc2);
+        System.out.println('Comptes: '  + client.getAccounts());
+    }
 }
+
+/*
+ * Dans la classe Client, ajoutez un attribut nommé accounts.
+ * 
+ * Ajoutez un accesseur pour cet attribut.
+ * 
+ * Un accesseur est une méthode qui retourne la valeur d'un attribut. On le
+ * nomme get suivi du nom de l'attribut avec la 1ère lettre en majuscule (voir
+ * exercice 4.1.1). Donc ici ce sera getAccounts
+ * 
+ * Complétez le constructeur pour initialiser accounts.
+ * 
+ * Ajoutez une méthode nommée addAccount qui ajoute un Account passé en
+ * paramètre a la liste des comptes du client.
+ */
