4d86cf339133dcc4f8d53347c7d97f9d58853d27
UserGitHubBeta <itslxc.agst@proton.me>
2023-03-13 18:58:49 +0100

    Delete Project

--- JAVA/MyConcerts/src/Ville.java
+++
@@ -1,9 +0,0 @@
-public class Ville {
-    String nom;
-    double nbMaxSpectateursAuMetreCarre;
-
-    public Ville(String nom, double nbMaxSpectateursAuMetreCarre) {
-        this.nom = nom;
-        this.nbMaxSpectateursAuMetreCarre = nbMaxSpectateursAuMetreCarre;
-    }
-}
